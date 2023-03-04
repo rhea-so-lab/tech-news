b7d9c284a6031795117ca94a5565fcb5061317fd
rhea-so <jeonghyeon.rhea@gmail.com>
2023-03-05 04:56:29 +0900

    [none] GitHub Action Workflow 추가

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import { EnvKey, EnvSafe } from '@creatrip/env-safe';
 
 @EnvSafe()
 export class Env {
-  @EnvKey({ default: 1000 * 60 * 60 * 24, description: '30분마다 RSS 확인' })
+  @EnvKey({ default: 1000 * 60 * 30, description: '30분마다 RSS 확인' })
   static SCHEDULE_TIME: number;
 
   @EnvKey()
